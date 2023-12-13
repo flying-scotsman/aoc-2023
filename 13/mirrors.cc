0b851b2b3d0d6685924ad2dea342948195d0e918
Matthew Green <mattgreen64@gmail.com>
2023-12-13 11:10:36 +0100

    Day 13 part 2

--- 13/mirrors.cc
+++ 13/mirrors.cc
@@ -57,6 +57,44 @@ std::vector<unsigned int> getColumnsLeftOfMirrorPlanes(std::vector<std::string> 
     return mirrorPlanes;
 }
 
+std::vector<unsigned int> getColumnsLeftOfMirrorPlanesWithSmudge(std::vector<std::string> const & block)
+{
+    std::vector<unsigned int> mirrorPlanes;
+    std::vector<unsigned int> mirrorPlaneDefects(block[0].size()-1, 0);
+
+    for (std::string const & line : block)
+    {
+        for (int i = 0; i < mirrorPlaneDefects.size(); ++i)
+        {
+            int l = i;
+            int r = i + 1;
+
+            while (l >= 0 && r < line.size())
+            {
+                if (line[l] != line[r])
+                {
+                    mirrorPlaneDefects[i]++;
+                }
+
+                l--;
+                r++;
+            }
+        }
+    }
+
+    // Any planes where there's only one defect are our guys
+    for (unsigned int i = 0; i < mirrorPlaneDefects.size(); ++i)
+    {
+        if (1 == mirrorPlaneDefects[i])
+        {
+            // std::cout << i << std::endl;
+            mirrorPlanes.push_back(i + 1);
+        }
+    }
+
+    return mirrorPlanes;
+}
+
 void solvePartOne(std::vector<std::string> const & lines)
 {
     // First split the lines into blocks (patterns)
@@ -78,7 +116,31 @@ void solvePartOne(std::vector<std::string> const & lines)
         }
     }
 
-    std::cout << "Summary of notes: " << summary << std::endl;
+    std::cout << "Part 1 summary: " << summary << std::endl;
+}
+
+void solvePartTwo(std::vector<std::string> const & lines)
+{
+    // First split the lines into blocks (patterns)
+    auto blocks{getTextBlocks(lines)};
+    unsigned int summary{0};
+    for (auto const & block : blocks)
+    {
+        // Then for each line in the block we check the symmetry of each plane
+        std::vector<unsigned int> mirrorPlanes{getColumnsLeftOfMirrorPlanesWithSmudge(block)};
+        for (unsigned int const p : mirrorPlanes)
+        {
+            summary += p;
+        }
+        // Then do the same with pivoted rows
+        std::vector<unsigned int> mirrorRows{getColumnsLeftOfMirrorPlanesWithSmudge(helpers::pivotLines(block))};
+        for (unsigned int const p : mirrorRows)
+        {
+            summary += 100 * p;
+        }
+    }
+
+    std::cout << "Part 2 summary: " << summary << std::endl;
 }
 
 int main(int argc, const char ** argv)
@@ -92,4 +154,5 @@ int main(int argc, const char ** argv)
     auto lines{helpers::getLinesInFile(argv[1])};
 
     solvePartOne(lines);
+    solvePartTwo(lines);
 }
