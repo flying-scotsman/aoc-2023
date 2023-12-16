e6f7d4dd03049fad2b7887baf0272202601e6ab4
Matthew Green <mattgreen64@gmail.com>
2023-12-16 13:27:27 +0100

    Day 15 part 2
    Using the delightful tsl::ordered_map

--- 15/hash.cc
+++ 15/hash.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <regex>
+#include <optional>
 #include "helpers.hpp"
+#include "tsl/ordered_map.h"
 
 std::vector<std::string> getWords(std::string const & sequence)
 {
-    // - Regex on absolutely anything but comma or newline
     std::vector<std::string> words{{}};
 
     for (char c : sequence)
@@ -17,9 +18,6 @@ std::vector<std::string> getWords(std::string const & sequence)
 
         words.back() += c;
     }
-
-    // for (auto w : words)
-    //     std::cout << w << std::endl;
 
     return words;
 }
@@ -52,6 +50,85 @@ void solvePartOne(std::string const & sequence)
     std::cout << "Total part 1: " << total << std::endl;
 }
 
+struct Code
+{
+    std::string letters;
+    unsigned int box;
+    char op;
+    std::optional<unsigned int> val;
+};
+
+Code getCode(std::string const & w)
+{
+    std::regex rgx{"[a-z]+"};
+    std::smatch letters;
+
+    if (!std::regex_search(w, letters, rgx))
+    {
+        std::cerr << "Couldn't find any letters in word!" << std::endl;
+        return {};
+    }
+
+    unsigned int box = hashAlgorithm(letters.str());
+    unsigned int op_pos = letters.position() + letters.str().size();
+    char op = w[op_pos];
+    std::optional<unsigned int> val{ std::nullopt };
+    if ('=' == op)
+        val = static_cast<unsigned int>(w[op_pos + 1] - '0');
+
+    return {letters.str(), box, op, val};
+}
+
+void solvePartTwo(std::string const & sequence)
+{
+    // Need a struct of string, operation and number - can split up words from part 1 for this and do it on the fly
+    // Box is given by running hashAlgorithm on letters
+    // Can I use an actual hashmap? The problem is it isn't sorted by entering, a vector makes much more sense
+    // But I need a fast lookup to remove elements
+    // I can use tsl::ordered_map for this :)
+
+    std::vector<tsl::ordered_map<std::string, unsigned int>> lensBoxes(256);
+
+    auto words{getWords(sequence)};
+
+    for (std::string const & w : words)
+    {
+        Code code{getCode(w)};
+
+        auto & thisMap{lensBoxes[code.box]};
+
+        if ('-' == code.op)
+        {
+            if (!thisMap.contains(code.letters))
+            {
+                // do nothing
+                continue;
+            }
+
+            // remove key
+            thisMap.erase(code.letters);
+        }
+        else if ('=' == code.op)
+        {
+            // assign to this value or add new one
+            thisMap[code.letters] = code.val.value();
+        }
+    }
+
+    unsigned int totalFocusingPower{0};
+    for (unsigned int i = 0; i < lensBoxes.size(); ++i)
+    {
+        unsigned int j = 1;
+        for (auto [key, value] : lensBoxes[i])
+        {
+            totalFocusingPower += (i + 1) * j * value;
+            j++;
+        }
+    }
+
+    std::cout << "Total focusing power: " << totalFocusingPower << std::endl;
+}
+
 int main(int argc, const char ** argv)
 {
     if (argc < 2)
@@ -62,5 +139,6 @@ int main(int argc, const char ** argv)
 
     auto lines{helpers::getLinesInFile(argv[1])};
 
-    solvePartOne(lines[0]);
+    // solvePartOne(lines[0]);
+    solvePartTwo(lines[0]);
 }
